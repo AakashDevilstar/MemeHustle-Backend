40a6405e8a00241fdadb57665d674499a1a66f81
AakashDevilstar <akashraturi2001@gmail.com>
2025-05-17 15:46:26 +0530

    Minor change

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ initSocket(io);
 app.use(cors());
 app.use(express.json());
 
+app.get('/',(req,res,next)=>{
+    res.status(200).json({
+      message:'success request'
+    })
+})
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use('/api/memes', memeRoutes);
 
 const PORT = process.env.PORT || 5000;
